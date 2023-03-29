231d2b66c4fd0287d1058e978a52622f954383ba
Lazarscorro <90773909+Lazarscorro@users.noreply.github.com>
2023-03-29 14:59:57 +0200

    links toegevoegd

--- app/views/homepages/index.php
+++ app/views/homepages/index.php
@@ -1,6 +1,5 @@
 <?php require APPROOT . '/views/includes/head.php'; ?>
 <p><h3><?= $data["title"]; ?></h3></p>
-<a href="<?=URLROOT;?>/students/index">Lessen student</a> | 
-<a href="<?=URLROOT;?>/students/annuleerLes">Lessen Annuleren</a> | 
-<a href="<?=URLROOT;?>/instructeur/index">Instructeurs in dienst</a>
+<a href="<?=URLROOT;?>/klanten/index">Overzicht van klanten</a>
+<a href="<?=URLROOT;?>/uitslagen/index">Overzicht van bowling uitslagen</a>
 <?php require APPROOT . '/views/includes/footer.php'; ?>
