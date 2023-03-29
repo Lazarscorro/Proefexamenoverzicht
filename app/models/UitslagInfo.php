957f92f2df9ac4bf5baaf2e3ce0b8ca0e8026dc5
Lazarscorro <90773909+Lazarscorro@users.noreply.github.com>
2023-03-29 16:53:12 +0200

    model uitslagen

---
+++ app/models/UitslagInfo.php
@@ -0,0 +1,35 @@
+<?php
+class KlantInfo
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function getKlantInfo()
+    {
+        $this->db->query("SELECT `Voornaam`, `Tussenvoegsel`, `Achternaam`, `IsVolwassen`, `Mobiel`, `Email` FROM `Persoon` INNER JOIN `Contact` ON Persoon.id = Contact.PersoonId;");
+
+        return $this->db->resultSet();;
+    }
+
+    public function getSingleKlantInfo($id)
+    {
+        $this->db->query("SELECT `Voornaam`, `Tussenvoegsel`, `Achternaam`, `IsVolwassen`, `Mobiel`, `Email` FROM `Persoon` INNER JOIN `Contact` ON Persoon.id = Contact.PersoonId WHERE `Id` = :id;");
+        $this->db->bind(':id', $id, PDO::PARAM_INT);
+
+        return $this->db->single();
+    }
+
+    
+    public function getUitslagInfo()
+    {
+        $this->db->query("SELECT `Voornaam`, `Tussenvoegsel`, `Achternaam`, `IsVolwassen`, `Mobiel`, `Email` FROM `Persoon` INNER JOIN `Contact` ON Persoon.id = Contact.PersoonId;");
+
+        return $this->db->resultSet();;
+    }
+
+}
+
